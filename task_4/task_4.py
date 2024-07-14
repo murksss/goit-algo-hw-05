584f15714b8c5d04cd0c4e9829de9c7655767f08
murksss <mshkepast@gmail.com>
2024-07-14 19:10:33 +0300

    Update task-4.py

--- task_4/task_4.py
+++ task_4/task_4.py
@@ -9,38 +9,39 @@ def main():
             if user_input:
                 command, *args = parse_input(user_input)
 
-                # Finish
-                if command in ['exit', 'close']:
-                    print(bot_answer('Goodbye!'))
-                    break
+                match command:
+                    # Finish
+                    case 'exit' | 'close':
+                        print(bot_answer('Goodbye!'))
+                        break
 
-                # Greeting
-                elif command in ['hi', 'hello']:
-                    print(bot_answer('Hello! How can I help you?'))
+                    # Greeting
+                    case 'hi' | 'hello':
+                        print(bot_answer('Hello! How can I help you?'))
 
-                # Add contact
-                elif command == 'add':
-                    print(add_contact(phone_book, *args))
+                    # Add contact
+                    case 'add':
+                        print(add_contact(phone_book, *args))
 
-                # Change contact
-                elif command == 'change':
-                    print(change_contact(phone_book, *args))
+                    # Change contact
+                    case 'change':
+                        print(change_contact(phone_book, *args))
 
-                # Show contact
-                elif command == 'phone':
-                    print(show_contact(phone_book, *args))
+                    # Show contact
+                    case 'phone':
+                        print(show_contact(phone_book, *args))
 
-                # Show all contact
-                elif command == 'all':
-                    print(show_all(phone_book))
+                    # Show all contact
+                    case 'all':
+                        print(show_all(phone_book))
 
-                # Show info
-                elif command == 'info':
-                    print(show_info())
+                    # Show info
+                    case 'info':
+                        print(show_info())
 
-                # Invalid command
-                else:
-                    print(bot_answer('I don\'t know how to handle this command.'))
+                    # Invalid command
+                    case _:
+                        print(bot_answer('I don\'t know how to handle this command.'))
 
     except KeyboardInterrupt:
         print(bot_answer('Oops.. Something went wrong..'))
